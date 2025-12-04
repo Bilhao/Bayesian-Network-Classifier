72eed4fcffbddfe52b02ddd8e65b0261f135fd83
Costinha05 <g.05.costasilva@gmail.com>
2025-12-04 23:29:53 +0000

    apenas testando as minhas grandes capacidades de programação

---
+++ src/App.java
@@ -0,0 +1,28 @@
+public class App {
+    int qi;
+
+    public App (int qi) {
+        this.qi = qi;
+    }
+    
+    public String inteligencia() {
+        if (qi < 90) {
+            return "É burro";
+        }
+        else {
+            return "É burro na mesma";
+        }
+    }
+
+    public String toString() {
+        return "Tem apenas " + qi + " de qi. Coitado, nasceu burro.";
+    }
+    
+    public static void main(String[] args) {
+        App rafael = new App(50);
+        System.out.println(rafael.inteligencia());
+        System.out.println(rafael);
+
+    }
+
+}
