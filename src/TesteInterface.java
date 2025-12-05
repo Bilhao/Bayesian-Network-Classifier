d9aa68d1854d9d73b4c7b98af95fbcefe8ad1cf1
Bilhão <rafaelr.bilhao@gmail.com>
2025-12-05 14:39:00 +0000

    Remoção de código inútil

--- src/TesteInterface.java
+++ src/TesteInterface.java
@@ -1,38 +1,37 @@
-import javax.swing.JFrame;
-import javax.swing.JTextArea;
+import java.awt.Color;
 
-import java.awt.EventQueue;
+import javax.swing.*;
 
 public class TesteInterface {
-    private JFrame frame;
 
     public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    TesteInterface window = new TesteInterface();
-                    window.frame.setVisible(true);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+        MyFrame mf = new MyFrame();
+        Label lb = new Label();
+        mf.add(lb);
+
     }
+}
 
-    public TesteInterface() {
-        initialize();
+class MyFrame extends JFrame {
+    MyFrame() {
+        this.setTitle("My Frame"); // Cria uma nova janela com o título "My Frame"
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Define a operação padrão de fechar a janela
+        // this.setResizable(false); // Define a janela como não redimensionável
+        this.setSize(500, 500);
+        this.setVisible(true);
+        // this.getContentPane().setBackground(Color.darkGray); // Define a cor de fundo
+        // da janela como azul
     }
+}
 
-    private void initialize() {
-        frame = new JFrame();
-        frame.setBounds(100, 100, 450, 300);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().setLayout(null);
-        
-        JTextArea textArea = new JTextArea();
-        textArea.setBounds(150, 80, 150, 100);
-        frame.getContentPane().add(textArea);
-
-        
+class Label extends JLabel {
+    Label() {
+        this.setText("Hello World"); // Define o texto do rótulo
+        this.setForeground(Color.black); // Define a cor do texto como preta
+        this.setHorizontalTextPosition(JLabel.CENTER); // Define a posição horizontal do texto em relação ao ícone
+        this.setVerticalTextPosition(JLabel.TOP); // Define a posição vertical do texto em relação ao ícone
+        this.setVerticalAlignment(JLabel.CENTER); // Define o alinhamento vertical do rótulo
+        this.setHorizontalAlignment(JLabel.CENTER); // Define o alinhamento horizontal do rótulo
+        // this.setBounds(100, 100, 250, 250); // Define a posição e o tamanho do rótulo
     }
 }
