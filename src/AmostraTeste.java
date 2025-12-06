f82920e1b47e8f9ea79050892db9b2efe44bc871
Costinha05 <g.05.costasilva@gmail.com>
2025-12-06 13:25:42 +0000

    quase acabado

--- src/AmostraTeste.java
+++ src/AmostraTeste.java
@@ -38,13 +38,44 @@ public class AmostraTeste {
     public int domain(int[] v) {
         int r = 1;
         for (int i = 0; i < v.length; i++) {
-            r = r * (this.max[i] + 1);
+            r = r * (this.max[i] + 1); //asumimos sempre a existência de valores intermédios
         }
         return r;
     }
 
-    public static void main(String[] args) {
+    public static boolean condQ(int[] var, int[] val, int[] x) { //recebe um array com referência às variáveis, um com os valores de cada variável, e uma amostra
+        boolean r = true;
+        for (int i = 0; i < var.length; i++) {
+            if (x[var[i]] != val[i]) { //se o valor da variável i da amostra x for diferente do valor para essa variável no vetor de valores, r = false
+                r = false;
+            }
+        }
+        return r; 
+    }
 
+    public int count(int[] var, int[] val) {
+        int r = 0;
+        for (int j = 0; j < this.length(); j++) {
+            int[] sample = this.element(j);
+            if (condQ(var, val, sample)) {
+                r++;
+            }
+        }
+        return r;
+    }
+
+    public String toString() {
+        return "Amostra = {Lista de amostras = " + valuesList + "], máximos = " + max + "]}";
+    }
+     
+
+    public static void main(String[] args) {
+        AmostraTeste m = new AmostraTeste();
+        int [] a1 = { 1, 2, 3, 4};
+        int [] a2 = { 0, 3, 4, 5};
+        m.add(a1);
+        m.add(a2);
+        System.out.println(m);
 
     }
 }
