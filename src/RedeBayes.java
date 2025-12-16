4aa6354d8b7abdac82700ca189a5cba34a816ef3
BalaFred7 <167338342+BalaFred7@users.noreply.github.com>
2025-12-16 19:25:33 +0000

    pronto ai está

---
+++ src/RedeBayes.java
@@ -0,0 +1,20 @@
+
+public class RedeBayes {
+
+    private OrientedGraph _grafo;
+
+    private Amostra _amostra;
+
+    public RedeBayes(OrientedGraph grafo, Amostra amostra) {
+        _grafo = grafo;
+        _amostra = amostra;
+    }
+ 
+    public RedeBayes BN(OrientedGraph grafo, Amostra amostra, double S) {
+        
+    }
+
+
+
+
+}
