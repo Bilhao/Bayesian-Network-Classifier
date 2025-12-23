3c3e6050fe79d5f9a5cdea89b3c40fe60ab5dd1e
BalaFred7 <167338342+BalaFred7@users.noreply.github.com>
2025-12-23 18:27:50 +0000

    Update BayesianNetwork.java

--- src/BayesianNetwork.java
+++ src/BayesianNetwork.java
@@ -3,18 +3,18 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 
-public class BayesianNetwork {
+public class BN {
     private Amostra amostra;
     private OrientedGraph grafo;
     private double S;
-    private int n;//número de variáveis (colunas)
+    private int n; //número de variáveis (colunas)
     private int C; //índice da variável classe no vetor de dados
     private double[] priorC; //array que guarda a distribuição de probabilidade da classe
     private Map<Integer, Map<String, double[]>> cpt;
     //cpt(conditional probability table) - tipo a memoria da rede
     // - recebe integer (indíce da varável Xi), devolve string e double[] (pais de Xi + valor da classe)
 
-    public BayesianNetwork(Amostra amostra, OrientedGraph grafo, double S) { //construtor
+    public BN(Amostra amostra, OrientedGraph grafo, double S) { //construtor
         this.amostra = amostra; //guarda dados
         this.grafo = grafo; //guarda o grafo que define a estrutura da rede
         this.S = S; //parâmetro das pseudo-contagens
@@ -35,7 +35,7 @@ public class BayesianNetwork {
         }
     }
 
-    private void buildCPT() {
+    private void buildCPT() { //aprende a probabilidade do resto
         for(int k = 0; k < C; k++) {
             int[] pais = grafo.parents(k);
             int tamanho = pais.length + 1;
@@ -52,11 +52,27 @@ public class BayesianNetwork {
                 int dk = amostra.max[k] + 1;
                 double[] probs = new double[dk];
                 for(int d = 0; d < dk; d++) {
-                    
+                    int[] vars = new int[paiscomclasse.length + 1];
+                    vars[0] = k;
+                    for (int j = 0; j < paiscomclasse.length; j++) {
+                    vars[j + 1] = paiscomclasse[j];
+                    }
+
+                    int[] vals = new int[w.length + 1];
+                    vals[0] = d;
+                    for (int j = 0; j < w.length; j++) {
+                    vals[j + 1] = w[j];
+                    }
+
+                    int numCount = amostra.count(vars, vals);
+                    double num = numCount + S;
+
+                    probs[d] = num / den;
+                    }
                 }
             }
         }
     }
 
 
-}
+
