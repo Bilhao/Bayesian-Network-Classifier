b9fb86ed24ab2693b246cd7b5ce7c7744317167a
simeaoversos <versosmaria@gmail.com>
2025-12-29 16:11:04 +0000

    RAFIM VE O QUE ACHAS DISTO

--- src/BN.java
+++ src/BN.java
@@ -23,56 +23,100 @@ public class BN {
         this.C = n-1;//valor da última coluna
         this.priorC = new double[amostra.max[C] + 1]; //+1 porque valores começam no 0
         this.cpt = new HashMap<>();//inicializa o mapa da cpt
+
+        buildPrior();
+        buildCPT();
     }
 
     private void buildPrior() { //aprende probabilidade da classe
         int N = amostra.length(); //número de amostras (linhas)
         double den = N + S*priorC.length; //denominador
         for(int k = 0; k < priorC.length; k++) {
-            double num = amostra.count(new int[]{C} ,new int[]{k}) + S;//numerador
-            double prob = num/den; //probabilidade
-            priorC[k] = prob;//guarda a probabilidade na posição k
+            int numCount = amostra.count(C, k);//numero de exemplos com classe k
+            double num =  numCount + S; //numerador
+            priorC[k] = num/den;//guarda a probabilidade na posição k
         }
     }
 
     private void buildCPT() { //aprende a probabilidade do resto
         for(int k = 0; k < C; k++) {
-            int[] pais = grafo.parents(k);
-            int tamanho = pais.length + 1;
-            int[] paiscomclasse = new int[tamanho];
-            for(int i = 0; i < pais.length; i++) { //coloca os pais no array
-                paiscomclasse[i] = pais[i];
+            ArrayList<Integer> pais = grafo.parents(k);
+            ArrayList<Integer> paiscomclasse = new ArrayList<>(pais);
+            paiscomclasse.add(C);
+            
+            ArrayList<ArrayList<Integer>> ws = amostra.combinations(paiscomclasse); //conjunto de combinações
+            Map<String, double[]> tabelaK = cpt.get(k);
+            if (tabelaK == null) {
+                tabelaK = new HashMap<>();
+                cpt.put(k, tabelaK);
             }
-            paiscomclasse[pais.length] = C; //adiciona a classe porque é sempre pai
-            ArrayList<int[]> ws = amostra.combinations(paiscomclasse); //conjunto de combinações
-            for(int i = 0; i < ws.size(); i++) {
-                int[] w = ws.get(i);//cada combinação 
-                int denCount = amostra.count(paiscomclasse, w);
-                double den = denCount + S*(amostra.max[k] + 1); //denominador
-                int dk = amostra.max[k] + 1;
-                double[] probs = new double[dk];
-                for(int d = 0; d < dk; d++) {
-                    int[] vars = new int[paiscomclasse.length + 1];
-                    vars[0] = k;
-                    for (int j = 0; j < paiscomclasse.length; j++) {
-                    vars[j + 1] = paiscomclasse[j];
-                    }
 
-                    int[] vals = new int[w.length + 1];
-                    vals[0] = d;
-                    for (int j = 0; j < w.length; j++) {
-                    vals[j + 1] = w[j];
-                    }
+            int dk = amostra.domain(k);
+            double Sk = S*dk;//S vezes a quantidade de valores possiveis
+            for(ArrayList<Integer> w:ws) {
+                int denCount = amostra.count(paiscomclasse, w);//ver quantas matches desses valores de pais existem iguais a w
+                double den = denCount + Sk; //denominador
+
+                double[] probs = new double[dk];
+            
+                ArrayList<Integer> vars = new ArrayList<>();
+                vars.add(k);
+                vars.addAll(paiscomclasse);
+                    
+                ArrayList<Integer> vals = new ArrayList<>();
+                vals.add(0);
+                vals.addAll(w);
+                for(int d = 0; d < dk; d++) {
+                    vals.set(0,d);
 
                     int numCount = amostra.count(vars, vals);
                     double num = numCount + S;
 
-                    probs[d] = num / den;
+                    probs[d] = num/den;
+                    
                     }
+                String key = w.toString();
+
+                tabelaK.put(key, probs);
                 }
             }
         }
+
+
+    public double prob(int[] x) {
+        int classe = x[C];
+        double p = priorC[classe];
+
+        for (int k = 0; k < C; k++) {
+            ArrayList<Integer> pais = grafo.parents(k);
+                
+            ArrayList<Integer> valores = new ArrayList<>();
+            for (int idx: pais) {
+                valores.add(x[idx]);
+            }
+            valores.add(classe);
+
+            String key = valores.toString();
+            double[] probs = cpt.get(k).get(key);
+            if (probs == null) return 0.0;
+            int val = x[k];
+
+            p *= probs[val];
+
+            }
+        return p;
+    
     }
+    
 
+    public static void main(String[] args){
+        Amostra a = ReadCSV.read("C:\\Users\\verso\\Downloads\\Bayesian-Network-Classifier-work\\Bayesian-Network-Classifier-work\\DataSets\\bcancer.csv");
+        OrientedGraph g = new OrientedGraph(a.element(0).length);
 
+        BN bn = new BN(a, g, 0.5);
 
+        int[] ex = a.element(0);
+
+        System.out.println(bn.prob(ex));
+    }
+}
