1560ecbf9dcb169ea4fd63b06652d0d8846d13a4
BalaFred7 <167338342+BalaFred7@users.noreply.github.com>
2025-12-30 17:20:13 +0000

    Update LOO.java

--- src/LOO.java
+++ src/LOO.java
@@ -1,12 +1,12 @@
 import java.util.Arrays;
 
-public class LOO {
+public class LOO { 
     public static double LeaveOneOut(Amostra amostra, double S) {
-        int acertos = 0;
+        int acertos = 0; //numero de classificação corretas
         int N = amostra.length(); //numero de linhas
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++) { //usar cada linha
             Amostra treino = new Amostra();
-            for (int j = 0; j < N; j++) {
+            for (int j = 0; j < N; j++) { //adiciona todas as linhas exceto a escolhida
                 if (j!=i) {
                     treino.add(amostra.element(j));
                 
@@ -14,10 +14,10 @@ public class LOO {
             }
             OrientedGraph grafoTeste = new OrientedGraph(amostra.element(0).length - 1);
             BN BNTeste = new BN(treino, grafoTeste, S);
-            int[] linhaTeste = amostra.element(i);
-            int[] xSemClasse = Arrays.copyOf(linhaTeste, linhaTeste.length - 1);
-            int classePrevista = BNTeste.classify(xSemClasse);
-            int classeReal = linhaTeste[linhaTeste.length -1];
+            int[] linhaTeste = amostra.element(i); //linha escolhida
+            int[] iSemClasse = Arrays.copyOf(linhaTeste, linhaTeste.length - 1); //remove a classe
+            int classePrevista = BNTeste.classify(iSemClasse);//classifica o exemplo teste
+            int classeReal = linhaTeste[linhaTeste.length -1]; //ultima coluna da linha
 
             if (classePrevista == classeReal) {
                 acertos ++;
